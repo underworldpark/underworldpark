c793816957ab0d35c90ee978425bbb790415c112
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2019-12-03 06:57:14 -0700

    fixed issues

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-<<<<<<< HEAD
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-150px";
-=======
-    document.getElementsByClassName("navbar").style.top = "0";
-  } else {
-    document.getElementByClassName("navbar").style.top = "-50px";
->>>>>>> 8b146b520d123ae00d7409d25e77c3b40b1f5b59
   }
   prevScrollpos = currentScrollPos;
 };
